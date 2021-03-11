d1a61550e88745eee387b24f83e6617176919fe0
Lucas Costa <lucasjcosta.98@gmail.com>
2021-03-11 13:59:00 -0300

    Added middleware code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, v4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: "" });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(id !== validate) {
+    return response.status(400).json({ error: "" });
+  }
+
+  if (!user) {
+    return response.status(404).json({ error: "" });
+  }
+
+  const todo = users.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "" });
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
